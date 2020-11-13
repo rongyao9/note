37f7ee24e14c21bd166a8cacfb1af9a0432d1377
dengrongyao <dengrongyao9@163.com>
2020-11-13 10:53:46 +0800

    replenish eventBus

--- src/eventBus使用/eventBus.js
+++ src/eventBus使用/eventBus.js
@@ -1,33 +1,45 @@
 const eventBus = {
-    names:[],
-    map:[],
-    init(){
-        let repeatName = this.names.filter((name,index)=>this.names.indexOf(name)!=index)
-        if(repeatName.length){
-             throw new Error(`存在重复事件${repeatName.join(',')}`)
+    names: [],
+    map: [],
+    init() {
+        let repeatName = this.names.filter((name, index) => this.names.indexOf(name) != index)
+        if (repeatName.length) {
+            throw new Error(`存在重复事件${repeatName.join(',')}`)
         }
     },
-    on(event){
-        if(!this.names.includes(event)){
-            throw new Error('设置的事件未初始化')
+    on(event, func, vue) {
+        if (!this.names.includes(event)) {
+            throw new Error('设置的事件未初始化，请前往eventBus.js记录事件并说明其用途')
         }
+
+        let sign = { func, vue }
+
+        if (this.map[event]) {
+            this.map[event].push(sign)
+        } else {
+            this.map[event] = [sign]
+        }
+        vue.$once('hook:beforeDestroy', () => {
+            this.map[event].splice(this.map[event].indexOf(sign), 1)
+        })
+
     },
-    emit(event,...args){
-        if(!this.map[event]) return 
-        this.map[event].forEach(sign=>{
+    emit(event, ...args) {
+        if (!this.map[event]) return;
+        this.map[event].forEach(sign => {
             sign.func(...args)
         })
     }
 }
 
 export default {
-    install(Vue){
+    install(Vue) {
         eventBus.init()
-        Vue.prototype.$listenEvent = (event,func)=>{
-            eventBus.on(event,func,this)
+        Vue.prototype.$listenEvent = (event, func) => {
+            eventBus.on(event, func, this)
         }
-        Vue.proptotype.$emitEvent = (rest)=>{
-            eventBus.emit(fun,...rest)
+        Vue.proptotype.$emitEvent = (rest) => {
+            eventBus.emit(fun, ...rest)
         }
     }
 }
